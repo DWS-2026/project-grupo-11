daeacd0290b0a0d492f4397ec827d2761cb3d7de
ruben2393 <ruben95.kfc@gmail.com>
2026-02-10 20:47:03 +0100

    completadas las clases Match, MatchEvent y Team

--- src/main/java/es/model/Team.java
+++ src/main/java/es/model/Team.java
@@ -1,5 +1,25 @@
 package es.model;
 
+import jakarta.persistence.*;
+import java.util.List;
+
 public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
     
+    @Lob
+    @Column(columnDefinition = "LONGBLOB")
+    private byte[] escudo;
+
+    // Campos para tu tabla classification.html
+    private int partidosJugados;
+    private int ganados;
+    private int empatados;
+    private int perdidos;
+    private int puntos;
+
+    // Getters y Setters
 }

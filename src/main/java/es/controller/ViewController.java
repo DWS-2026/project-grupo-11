ea343fb57cd5d46481c6fc048f8f4f2b0d71bf64
xxSerbotXX04 <sergitosan04@gmail.com>
2026-02-10 20:49:37 +0100

    Modificado Controller

---
+++ src/main/java/es/controller/ViewController.java
@@ -0,0 +1,29 @@
+package es.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // si tienes index.html
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "registration";
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "profile";
+    }
+}
+

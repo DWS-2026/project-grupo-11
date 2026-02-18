ef56ccd12d9044814363a7723db1531ca487c32b
xxSerbotXX04 <sergitosan04@gmail.com>
2026-02-18 15:18:02 +0100

    MatchEventService creado

---
+++ src/main/java/es/footleague/app/services/MatchEventService.java
@@ -0,0 +1,39 @@
+package es.footleague.app.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import es.footleague.app.model.MatchEvent;
+import es.footleague.app.repository.MatchEventRepository;
+
+@Service
+public class MatchEventService {
+
+    @Autowired
+    private MatchEventRepository matchEventRepository;
+
+    public List<MatchEvent> findAll(){
+        return matchEventRepository.findAll();
+    }
+
+    public void save(MatchEvent event){
+        if(event.getMinute() < 0 || event.getMinute() > 120){
+            throw new IllegalArgumentException("El minuto debe estar entre 0 y 120");
+        }
+        else{
+           matchEventRepository.save(event);
+        }
+    }
+
+    public Optional<MatchEvent> findById(long id){
+        return matchEventRepository.findById(id);
+    }
+
+    public void deleteById(long id) {
+        matchEventRepository.deleteById(id);
+	}
+
+}

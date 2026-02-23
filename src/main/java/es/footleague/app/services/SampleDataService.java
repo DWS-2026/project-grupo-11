052de9f6543fa52ecbc2957103cedddd408c200b
ruben2393 <ruben95.kfc@gmail.com>
2026-02-23 20:52:03 +0100

    cambio sin importancia

--- src/main/java/es/footleague/app/services/SampleDataService.java
+++ src/main/java/es/footleague/app/services/SampleDataService.java
@@ -1,7 +1,6 @@
 package es.footleague.app.services;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 import org.springframework.beans.factory.annotation.Autowired;

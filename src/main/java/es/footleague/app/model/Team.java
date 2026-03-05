5817388e6c5dcc60a3b0208b28b5fe17b3802180
ruben2393 <ruben95.kfc@gmail.com>
2026-03-05 22:11:57 +0100

    cambiados detalles de la clasificacion

--- src/main/java/es/footleague/app/model/Team.java
+++ src/main/java/es/footleague/app/model/Team.java
@@ -20,7 +20,16 @@ public class Team {
     private int points;
 
     @Column(nullable = false)
-    private int goals;
+    private int playedMatchs;
+
+    @Column(nullable = false)
+    private int wins;
+
+    @Column(nullable = false)
+    private int draws;
+
+    @Column(nullable = false)
+    private int losses;
 
     @Lob
     @Column(name = "logo_data", columnDefinition = "LONGBLOB")
@@ -28,15 +37,16 @@ public class Team {
 
     // --- CAMBIO AQUÍ: De protected a public ---
     public Team() {
-        this.points = 0;
-        this.goals = 0;
     }
 
     public Team(String name, String stadiumName) {
         this.name = name;
         this.stadiumName = stadiumName;
         this.points = 0;
-        this.goals = 0;
+        this.playedMatchs = 0;
+        this.wins = 0;
+        this.draws = 0;
+        this.losses = 0;
     }
 
     /**
@@ -50,16 +60,75 @@ public class Team {
     }
 
     // --- GETTERS Y SETTERS ---
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getStadiumName() { return stadiumName; }
-    public void setStadiumName(String stadiumName) { this.stadiumName = stadiumName; }
-    public byte[] getLogoData() { return logoData; }
-    public void setLogoData(byte[] logoData) { this.logoData = logoData; }
-    public int getPoints() { return points; }
-    public void setPoints(int points) { this.points = points; }
-    public int getGoals() { return goals; }
-    public void setGoals(int goals) { this.goals = goals; }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStadiumName() {
+        return stadiumName;
+    }
+
+    public void setStadiumName(String stadiumName) {
+        this.stadiumName = stadiumName;
+    }
+
+    public byte[] getLogoData() {
+        return logoData;
+    }
+
+    public void setLogoData(byte[] logoData) {
+        this.logoData = logoData;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public int getPlayedMatches() {
+        return this.playedMatchs;
+    }
+
+    public void setPlayedMatches(int playedMatches) {
+        this.playedMatchs = playedMatches;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getDraws() {
+        return draws;
+    }
+
+    public void setDraws(int draws) {
+        this.draws = draws;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
+    }
 }

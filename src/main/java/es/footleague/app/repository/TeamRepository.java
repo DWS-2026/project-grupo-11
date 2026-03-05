a063cfa66f7055f87972adc3fe6abbebb87f8766
ruben2393 <ruben95.kfc@gmail.com>
2026-03-05 17:57:19 +0100

    modificadas las paginas de classification e index, y el teamRepository

--- src/main/java/es/footleague/app/repository/TeamRepository.java
+++ src/main/java/es/footleague/app/repository/TeamRepository.java
@@ -1,9 +1,13 @@
 package es.footleague.app.repository;
 
 import es.footleague.app.model.Team;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
+    List<Team> findAllByOrderByPointsDesc();
 }

55a806bb6c82294531020b2219df866e400010bc
Jaime2006cib <jaimetejerofernandez@gmail.com>
2026-03-17 10:23:44 +0100

    Cambiadas ruta errores

--- src/main/java/es/footleague/app/controller/UserController.java
+++ src/main/java/es/footleague/app/controller/UserController.java
@@ -78,6 +78,26 @@ public class UserController {
         }
         return "user_not_found";
     }
+    //Temporal 
+    
+    @GetMapping("/400")
+    public String view400() { return "error/400"; } // Pase Impreciso
+
+    @GetMapping("/403")
+    public String view403() { return "error/403"; } // Zona Exclusiva
+
+    @GetMapping("/404")
+    public String view404() { return "error/404"; } // Fuera de Juego
+
+    @GetMapping("/409")
+    public String view409() { return "error/409"; } // Jugada Repetida
+
+    @GetMapping("/500")
+    public String view500() { return "error/500"; } // Tarjeta Roja
+
+    @GetMapping("/503")
+    public String view503() { return "error/503"; } // En el Banquillo
+    //
 
     // 2. REGISTRATION FORM (View)
     @GetMapping("/register")
